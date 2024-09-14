6c63c464698799754f3832017074a7c5945c41f3
k1_517_01 <k1_517_01@K1-517-01>
2024-09-14 11:39:56 +0300

    Добавьте файлы проекта.

---
+++ OOP_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Serialization;
+namespace OOP_1;
+class Program()
+{
+    static void Main(string[] args)
+    {
+        // Используем конструктор, принимающий только имя
+        Student student1 = new Student("Иван");
+        Console.WriteLine(student1.WriteInfo()); // Вывод: Имя: Иван, Возраст: 0
+
+        // Используем конструктор, принимающий имя и возраст
+        Student student2 = new Student("Мария", 20);
+        Console.WriteLine(student2.WriteInfo()); // Вывод: Имя: Мария, Возраст: 20
+
+        // Увеличиваем возраст первого студента
+        student1.BecomeOlder();
+        Console.WriteLine(student1.WriteInfo()); // Вывод: Имя: Иван, Возраст: 1
+
+        // Увеличиваем возраст второго студента
+        student2.BecomeOlder();
+        Console.WriteLine(student2.WriteInfo()); // Вывод: Имя: Мария, Возраст: 21
+    }
+}
+
---
+++ OOP_1/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_1
+{
+    public class Student
+    {
+        private string _name;
+        public int Age { get; set; }
+
+        // Свойство для имени
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        // Конструктор, принимающий только имя
+        public Student(string name)
+        {
+            _name = name;
+            Age = 0;
+        }
+
+        // Конструктор, принимающий имя и возраст
+        public Student(string name, int age)
+        {
+            _name = name;
+            Age = age;
+        }
+
+        // Метод для получения информации о студенте
+        public string WriteInfo()
+        {
+            return $"Имя: {_name}, Возраст: {Age}";
+        }
+
+        // Метод для увеличения возраста на единицу
+        public void BecomeOlder()
+        {
+            Age++;
+        }
+    }
+}
