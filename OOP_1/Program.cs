7a348761260c682c288531fe9a583837c53d236d
vskforeva <sergeynmnm@gmail.com>
2024-09-20 15:11:55 +0300

    Добавлены задания из Лабораторная №2

--- OOP_1/Program.cs
+++ OOP_1/Program.cs
@@ -6,19 +6,37 @@ class Program()
     {
         // Используем конструктор, принимающий только имя
         Student student1 = new Student("Иван");
-        Console.WriteLine(student1.WriteInfo()); // Вывод: Имя: Иван, Возраст: 0
+        Subject favoriteSubject1 = new Subject("Программирование");
+        Game favoriteGame1 = new Game("Футбол");
+        student1.SetFavorites(favoriteSubject1, favoriteGame1);
+        Console.WriteLine(student1.WriteInfo()); // Вывод: Имя: Иван, Возраст: 0, Программ-е, Футбол
 
         // Используем конструктор, принимающий имя и возраст
         Student student2 = new Student("Мария", 20);
-        Console.WriteLine(student2.WriteInfo()); // Вывод: Имя: Мария, Возраст: 20
+        Subject favoriteSubject2 = new Subject("Математика");
+        Game favoriteGame2 = new Game("Шахматы");
+        student2.SetFavorites(favoriteSubject2, favoriteGame2);
+        Console.WriteLine(student2.WriteInfo()); // Вывод: Имя: Мария, Возраст: 20, Математика, Шахматы
+
+        Console.WriteLine(" ");
 
         // Увеличиваем возраст первого студента
         student1.BecomeOlder();
-        Console.WriteLine(student1.WriteInfo()); // Вывод: Имя: Иван, Возраст: 1
+        Console.WriteLine(student1.WriteInfo()); // Вывод: Имя: Иван, Возраст: 1, Программ-е, Футбол
 
         // Увеличиваем возраст второго студента
         student2.BecomeOlder();
-        Console.WriteLine(student2.WriteInfo()); // Вывод: Имя: Мария, Возраст: 21
+        Console.WriteLine(student2.WriteInfo()); // Вывод: Имя: Мария, Возраст: 21, Математика, Шахматы
+
+        Console.WriteLine(" ");
+
+        // Демонстрация передачи параметров
+        string nameValue = "Оригинальное имя";
+        student2.ChangeNameByValue(nameValue);
+        Console.WriteLine($"Имя после вызова ChangeNameByValue: {nameValue}");
+
+        student2.ChangeNameByReference(ref nameValue);
+        Console.WriteLine($"Имя после вызова ChangeNameByReference: {nameValue}");
     }
 }
 
--- OOP_1/Student.cs
+++ OOP_1/Student.cs
@@ -11,6 +11,10 @@ namespace OOP_1
         private string _name;
         public int Age { get; set; }
 
+        // Свойства для любимого предмета и игры
+        public Subject FavoriteSubject { get; private set; }
+        public Game FavoriteGame { get; private set; }
+
         // Свойство для имени
         public string Name
         {
@@ -32,16 +36,55 @@ namespace OOP_1
             Age = age;
         }
 
+        // Метод для установки любимого предмета и игры
+        public void SetFavorites(Subject subject, Game game)
+        {
+            FavoriteSubject = subject;
+            FavoriteGame = game;
+        }
+
         // Метод для получения информации о студенте
         public string WriteInfo()
         {
-            return $"Имя: {_name}, Возраст: {Age}";
+            return $"Имя: {_name}, Возраст: {Age}, Любимый предмет: {FavoriteSubject?.Name}, Любимая игра: {FavoriteGame?.Name}";
         }
 
         // Метод для увеличения возраста на единицу
         public void BecomeOlder()
         {
             Age++;
+        }
+
+        // Метод для демонстрации передачи параметра по значению и по ссылке
+        public void ChangeNameByValue(string newName)
+        {
+            newName = "Измененное имя";
+            Console.WriteLine($"Имя внутри метода (по значению): {newName}");
+        }
+
+        public void ChangeNameByReference(ref string newName)
+        {
+            newName = "Измененное имя";
+            Console.WriteLine($"Имя внутри метода (по ссылке): {newName}");
+        }
+    }
+    public class Subject
+    {
+        public string Name { get; set; }
+
+        public Subject(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class Game
+    {
+        public string Name { get; set; }
+
+        public Game(string name)
+        {
+            Name = name;
         }
     }
 }
